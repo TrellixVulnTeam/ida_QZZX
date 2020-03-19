1ee051dab9d539386c990e072ea783d1328caf2c
Moritz Renftle <moritz.renftle@student.kit.edu>
2020-03-19 19:41:30 +0100

    Allow to specify batch size with pytest.

--- src/thesis_impl/places365/conftest.py
+++ src/thesis_impl/places365/conftest.py
@@ -1,9 +1,17 @@
 
 def pytest_addoption(parser):
     parser.addoption("--validation-path", action="store")
+    parser.addoption("--batch-size", type=int, default=512, action="store")
 
 
 def pytest_generate_tests(metafunc):
-    option_value = metafunc.config.option.validation_path
-    if 'validation_path' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("validation_path", [option_value])
+    validation_path = metafunc.config.option.validation_path
+    batch_size = metafunc.config.option.batch_size
+
+    if 'validation_path' in metafunc.fixturenames \
+            and validation_path is not None:
+        metafunc.parametrize("validation_path", [validation_path])
+
+    if 'batch_size' in metafunc.fixturenames \
+            and batch_size is not None:
+        metafunc.parametrize("batch_size", [batch_size])
--- src/thesis_impl/places365/test_hub.py
+++ src/thesis_impl/places365/test_hub.py
@@ -21,7 +21,7 @@ def test_that_resnet18_works():
         assert hub.all_labels[label_ids[0]] == 'food court'
 
 
-def test_resnet18_accuracy(caplog, validation_path):
+def test_resnet18_top_k_accuracy(caplog, validation_path, batch_size, k=5):
     caplog.set_level(logging.INFO)
 
     hub = Places365Hub()
@@ -29,7 +29,7 @@ def test_resnet18_accuracy(caplog, validation_path):
 
     val = torchvision.datasets.ImageFolder(root=validation_path,
                                            transform=resnet18.transform_image)
-    val_loader = torch.utils.data.DataLoader(val, batch_size=512,
+    val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size,
                                              num_workers=6,
                                              pin_memory=False,
                                              shuffle=True)
@@ -40,89 +40,36 @@ def test_resnet18_accuracy(caplog, validation_path):
     num_processed = 0
     total_acc = 0
 
-    with torch.no_grad():
-        for data in val_loader:
-            images, labels = data
-            probs = resnet18.predict_probabilities(images)
-            _, predicted = torch.max(probs, -1)
-            c = (predicted == labels).squeeze()
+    try:
+        with torch.no_grad():
+            for data in val_loader:
+                images, labels = data
+                probs = resnet18.predict_probabilities(images)
+                _, predicted = torch.topk(probs, k, -1)
+                k_labels = labels.unsqueeze(-1).expand_as(predicted)
+                c = (predicted == k_labels).sum(-1)
 
-            for img_count, label_id in enumerate(labels):
-                correct_of_label[label_id] += c[img_count].item()
-                total_of_label[label_id] += 1
+                for img_count, label_id in enumerate(labels):
+                    correct_of_label[label_id] += c[img_count].item()
+                    total_of_label[label_id] += 1
 
-                num_processed += 1
+                    num_processed += 1
 
-            logging.info('Processed {} images so far.'.format(num_processed))
-            total_acc = (correct_of_label.sum() / total_of_label.sum())\
-                .squeeze()
-            logging.info('Current accuracy is {}.'.format(total_acc.item()))
-
-            if num_processed >= 1024:
-                break
-
-    acc_of_label = correct_of_label / total_of_label
-    acc_of_label_names = '\n'.join(['{}: {}'.format(l, acc.item())
-                                   for l, acc in zip(hub.all_labels,
-                                                     acc_of_label)])
-
-    logging.info('----- FINISHED PROCESSING VALIDATION DATA -----')
-    logging.info('Accuracy of individual labels:\n{}'
-                 .format(acc_of_label_names))
-    logging.info('Total accuracy: {}'.format(total_acc.item()))
-
-    assert total_acc > 0.7
-
-
-def test_resnet18_top_k_accuracy(caplog, validation_path, k=5):
-    caplog.set_level(logging.INFO)
-
-    hub = Places365Hub()
-    resnet18 = hub.resnet18()
-
-    val = torchvision.datasets.ImageFolder(root=validation_path,
-                                           transform=resnet18.transform_image)
-    val_loader = torch.utils.data.DataLoader(val, batch_size=16,
-                                             num_workers=6,
-                                             pin_memory=False,
-                                             shuffle=True)
-
-    correct_of_label = torch.zeros(len(hub.all_labels))
-    total_of_label = torch.zeros(len(hub.all_labels))
-
-    num_processed = 0
-    total_acc = 0
-
-    with torch.no_grad():
-        for data in val_loader:
-            images, labels = data
-            probs = resnet18.predict_probabilities(images)
-            _, predicted = torch.topk(probs, k, -1)
-            k_labels = labels.unsqueeze(-1).expand_as(predicted)
-            c = (predicted == k_labels).sum(-1)
-
-            for img_count, label_id in enumerate(labels):
-                correct_of_label[label_id] += c[img_count].item()
-                total_of_label[label_id] += 1
-
-                num_processed += 1
-
-            logging.info('Processed {} images so far.'.format(num_processed))
-            total_acc = (correct_of_label.sum() / total_of_label.sum())\
-                .squeeze()
-            logging.info('Current accuracy is {}.'.format(total_acc.item()))
-
-            if num_processed >= 1024:
-                break
+                logging.info('Processed {} images so far.'.format(num_processed))
+                total_acc = (correct_of_label.sum() / total_of_label.sum())\
+                    .squeeze()
+                logging.info('Current accuracy is {}.'.format(total_acc.item()))
+    except KeyboardInterrupt:
+        logging.info('---- ! Stopping due to KeyboardInterrupt ! ----')
+    else:
+        logging.info('----- Finished -----')
 
     acc_of_label = correct_of_label / total_of_label
     acc_of_label_names = '\n'.join(['{}: {}'.format(l, acc.item())
                                    for l, acc in zip(hub.all_labels,
                                                      acc_of_label)])
 
-    logging.info('----- FINISHED PROCESSING VALIDATION DATA -----')
     logging.info('Accuracy of individual labels:\n{}'
                  .format(acc_of_label_names))
     logging.info('Total accuracy: {}'.format(total_acc.item()))
 
-    assert total_acc > 0.7
