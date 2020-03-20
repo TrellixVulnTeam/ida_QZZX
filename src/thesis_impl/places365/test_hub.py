f4dda83439eb60db0cdf585b026383ca9e45bddd
Moritz Renftle <moritz.renftle@student.kit.edu>
2020-03-20 19:45:39 +0100

    Add test for indoor/outdoor classification accuracy.

--- src/thesis_impl/places365/test_hub.py
+++ src/thesis_impl/places365/test_hub.py
@@ -1,5 +1,6 @@
 import logging
 
+import pytest
 import torchvision
 import torch
 from torch.autograd import Variable as V
@@ -21,9 +22,8 @@ def test_that_resnet18_works():
         assert hub.all_labels[label_ids[0]] == 'food court'
 
 
-def test_resnet18_top_k_accuracy(caplog, validation_path, batch_size, k=5):
-    caplog.set_level(logging.INFO)
-
+@pytest.fixture
+def resnet18_evaluation(validation_path, batch_size):
     hub = Places365Hub()
     resnet18 = hub.resnet18()
 
@@ -33,43 +33,83 @@ def test_resnet18_top_k_accuracy(caplog, validation_path, batch_size, k=5):
                                              num_workers=6,
                                              pin_memory=False,
                                              shuffle=True)
+    return hub, resnet18, val_loader
 
-    correct_of_label = torch.zeros(len(hub.all_labels))
-    total_of_label = torch.zeros(len(hub.all_labels))
 
-    num_processed = 0
-    total_acc = 0
+def _evaluate_on_validation_set(caplog, hub, val_loader, method):
+    outer_level = logging.getLogger().level
 
-    try:
-        with torch.no_grad():
-            for data in val_loader:
-                images, labels = data
-                probs = resnet18.predict_probabilities(images)
-                _, predicted = torch.topk(probs, k, -1)
-                k_labels = labels.unsqueeze(-1).expand_as(predicted)
-                c = (predicted == k_labels).sum(-1)
+    with caplog.at_level(logging.INFO):
+        correct_of_label = torch.zeros(len(hub.all_labels))
+        total_of_label = torch.zeros(len(hub.all_labels))
 
-                for img_count, label_id in enumerate(labels):
-                    correct_of_label[label_id] += c[img_count].item()
-                    total_of_label[label_id] += 1
+        num_processed = 0
+        total_acc = 0
 
-                    num_processed += 1
+        try:
+            with torch.no_grad():
+                for data in val_loader:
+                    images, labels = data
+                    num_processed += images.size()[0]
 
-                logging.info('Processed {} images so far.'.format(num_processed))
-                total_acc = (correct_of_label.sum() / total_of_label.sum())\
-                    .squeeze()
-                logging.info('Current accuracy is {}.'.format(total_acc.item()))
-    except KeyboardInterrupt:
-        logging.info('---- ! Stopping due to KeyboardInterrupt ! ----')
-    else:
-        logging.info('----- Finished -----')
+                    with caplog.at_level(outer_level):
+                        for label_id, prediction_is_correct \
+                                in method(images, labels):
+                            if prediction_is_correct:
+                                correct_of_label[label_id] += 1
+                            total_of_label[label_id] += 1
 
-    acc_of_label = correct_of_label / total_of_label
-    acc_of_label_names = '\n'.join(['{}: {}'.format(l, acc.item())
-                                   for l, acc in zip(hub.all_labels,
-                                                     acc_of_label)])
+                    logging.info('Processed {} images so far.'
+                                 .format(num_processed))
+                    total_acc = (correct_of_label.sum() / total_of_label.sum())\
+                        .squeeze()
+                    logging.info('Current accuracy is {}.'.format(total_acc.item()))
+        except KeyboardInterrupt:
+            logging.info('---- ! Stopping due to KeyboardInterrupt ! ----')
+        else:
+            logging.info('----- Finished -----')
 
-    logging.info('Accuracy of individual labels:\n{}'
-                 .format(acc_of_label_names))
-    logging.info('Total accuracy: {}'.format(total_acc.item()))
+        acc_of_label = correct_of_label / total_of_label
+        acc_of_label_names = '\n'.join(['{}: {}'.format(l, acc.item())
+                                       for l, acc in zip(hub.all_labels,
+                                                         acc_of_label)])
 
+        logging.info('Accuracy of individual labels:\n{}'
+                     .format(acc_of_label_names))
+        logging.info('Total accuracy: {}'.format(total_acc.item()))
+
+
+def test_resnet18_top_k_accuracy(caplog, resnet18_evaluation, k=5):
+    hub, resnet18, val_loader = resnet18_evaluation
+
+    def _eval(images, labels):
+        probs = resnet18.predict_probabilities(images)
+        _, predicted_labels = torch.topk(probs, k, -1)
+        true_labels = labels.unsqueeze(-1).expand_as(predicted_labels)
+        success = (predicted_labels == true_labels).sum(-1)
+
+        for pred_no, label_id in enumerate(labels):
+            yield label_id, bool(success[pred_no].item())
+
+    _evaluate_on_validation_set(caplog, hub, val_loader, _eval)
+
+
+def test_resnet18_indoor_outdoor_accuracy(caplog, resnet18_evaluation, k=10):
+    hub, resnet18, val_loader = resnet18_evaluation
+
+    def _eval(images_batch, labels_batch):
+        probs_batch = resnet18.predict_probabilities(images_batch)
+        _, k_predicted_labels_batch = torch.topk(probs_batch, k, -1)
+
+        for true_label_id, k_predicted_labels \
+                in zip(labels_batch, k_predicted_labels_batch):
+            predict_outdoor = hub.vote_indoor_outdoor(k_predicted_labels)
+            is_outdoor = hub.is_outdoor(true_label_id)
+
+            logging.info('Scene "{}" is {}. Predicted as {}.'
+                         .format(hub.label_name(true_label_id), is_outdoor,
+                                 predict_outdoor))
+
+            yield true_label_id, predict_outdoor == is_outdoor
+
+    _evaluate_on_validation_set(caplog, hub, val_loader, _eval)
