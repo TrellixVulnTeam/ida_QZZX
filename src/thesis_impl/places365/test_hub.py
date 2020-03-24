a3e55d0bff566de943c34749025a69c7a532a4f5
Moritz Renftle <moritz.renftle@student.kit.edu>
2020-03-24 22:07:02 +0100

    Fix image loading.

--- src/thesis_impl/places365/test_hub.py
+++ src/thesis_impl/places365/test_hub.py
@@ -5,8 +5,8 @@ import torch
 from petastorm import make_reader
 from petastorm.pytorch import DataLoader
 from PIL import Image
+from torchvision.transforms.functional import to_tensor
 
-from thesis_impl.places365 import wideresnet
 from thesis_impl.places365.hub import Places365Hub
 
 
@@ -14,13 +14,17 @@ _DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 logging.info('Torch is using device {}.'.format(_DEVICE))
 
 
+def _load_image_as_tensor(file_path, size=(224, 224)):
+    image = Image.open(file_path).resize(size)
+    return to_tensor(image)
+
+
 def test_that_resnet18_works():
     hub = Places365Hub()
     resnet18 = hub.resnet18()
 
     with hub.open_demo_image() as demo_file:
-        image = Image.open(demo_file)
-        image_tensor = wideresnet.IMAGE_TRANSFORM(image).unsqueeze(0)
+        image_tensor = _load_image_as_tensor(demo_file).unsqueeze(0)
         label_probs = resnet18.predict_probabilities(image_tensor)
         _, label_ids = label_probs.sort(-1, True)
 
