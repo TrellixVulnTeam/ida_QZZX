1faec079bf55d21adf303453a11841f7dc1500f5
Moritz Renftle <moritz.renftle@student.kit.edu>
2020-04-05 00:16:17 +0200

    Reduce logging spam by tf. Fix: Allow having multiple translators of the same tensor type. Add translator that computes a "color distribution". Fix: Clone tensors before model inference, otherwise tensors would be changed in place, leading to corruption in subsequent translators.

--- src/thesis_impl/places365/translate.py
+++ src/thesis_impl/places365/translate.py
@@ -22,14 +22,16 @@ from petastorm.tf_utils import make_petastorm_dataset
 from petastorm.unischema import UnischemaField, dict_to_spark_row, Unischema
 
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import IntegerType
-
-import tensorflow as tf
+from pyspark.sql.types import IntegerType, FloatType
 
 from thesis_impl.places365.hub import Places365Hub
 from thesis_impl.places365.io import unsupervised_loader
 import thesis_impl.places365.config as cfg
 from thesis_impl.util.webcache import WebCache
+
+import tensorflow as tf
+tf.get_logger().setLevel(logging.ERROR)  # avoid log spamming by tf
+tf.autograph.set_verbosity(1)
 
 
 class Translator(Callable, abc.ABC):
@@ -59,10 +61,70 @@ class ToImageDummyTranslator(Translator):
         return [self._field]
 
     def __call__(self, image_tensors):
-        for image_tensor in image_tensors.cpu():
-            image_arr = np.asarray(to_pil_image(image_tensor, 'RGB')
-                                   .resize((self._width, self._height)))
-            yield {self._field.name: image_arr}
+        with torch.no_grad():
+            for image_tensor in image_tensors.cpu():
+                image_arr = np.asarray(to_pil_image(image_tensor, 'RGB')
+                                       .resize((self._width, self._height)))
+                yield {self._field.name: image_arr}
+
+
+class ToColorDistributionTranslator(Translator):
+
+    # assign natural language color names to hue values
+    COLOR_MAP = {20.: 'red',
+                 45.: 'orange',
+                 55.: 'gold',
+                 65.: 'yellow',
+                 155.: 'green',
+                 185.: 'turquoise',
+                 250.: 'blue',
+                 280.: 'purple',
+                 320.: 'magenta',
+                 360.: 'red'}
+
+    def __init__(self, num_colors: int=10, resize_to: int=100):
+        super().__init__('torch', 'distribution of colors in the image')
+        self.num_colors = num_colors
+
+        self._color_bins = self.COLOR_MAP.values()
+        self._colors = set(self._color_bins)
+        self._hue_bins = np.array([0.] + list(self.COLOR_MAP.keys())) \
+            / 360. * 255.
+
+        self._fields = [UnischemaField(self._get_field_name(color_name),
+                                       float, (), ScalarCodec(FloatType()),
+                                       False)
+                        for color_name in self._colors]
+
+        self._resize_to = resize_to
+
+    def _get_field_name(self, color_name):
+        return 'fraction_of_{}_pixels'.format(color_name)
+
+    @property
+    def fields(self) -> [UnischemaField]:
+        return self._fields
+
+    def __call__(self, image_tensors):
+        with torch.no_grad():
+            for image_tensor in image_tensors.cpu():
+                image = to_pil_image(image_tensor, 'RGB')\
+                    .resize((self._resize_to, self._resize_to))\
+                    .convert('HSV')
+
+                image_hues = np.asarray(image)[:, :, 0].flatten()
+                hue_fractions, _ = np.histogram(image_hues, self._hue_bins,
+                                                range(256))
+                # we want a fraction of the total pixels
+                hue_fractions = hue_fractions / (self._resize_to ** 2)
+
+                color_fractions = {color: .0 for color in self._colors}
+
+                for hue_fraction, color in zip(hue_fractions, self._color_bins):
+                    color_fractions[color] += hue_fraction
+
+                yield {self._get_field_name(color): fraction
+                       for color, fraction in color_fractions.items()}
 
 
 def _wrap_parallel(model):
@@ -99,12 +161,14 @@ class ToPlaces365SceneNameTranslator(Translator):
         return ToPlaces365SceneNameTranslator(model, hub=hub, top_k=top_k)
 
     def __call__(self, image_tensors):
+        image_tensors = image_tensors.clone().detach()
+
         with torch.no_grad():
             probs = self.model(image_tensors)
             _, predicted_labels_batch = torch.topk(probs, self._top_k, -1)
             for predicted_labels in predicted_labels_batch:
-                yield {self._get_field_name(i): predicted_labels[i].cpu().item()
-                       for i in range(self._top_k)}
+                yield {f.name: predicted_labels[i].cpu().item()
+                       for i, f in enumerate(self._fields)}
 
 
 class ToCocoObjectNamesTranslator(Translator):
@@ -165,10 +229,12 @@ class ToCocoObjectNamesTranslator(Translator):
                 for obj_id, count in enumerate(counts_array) if count > 0}
 
     def __call__(self, image_tensors):
+        image_tensors = image_tensors.clone().detach()
+
         with torch.no_grad():
             for result in self.model(image_tensors):
                 obj_ids, scores = result['labels'], result['scores']
-                obj_counts = Counter(obj_id.cpu().detach().item()
+                obj_counts = Counter(obj_id.cpu().item()
                                      for i, obj_id in enumerate(obj_ids)
                                      if scores[i] > self.threshold)
 
@@ -244,6 +310,7 @@ class ToOIV4ObjectNameTranslator(Translator):
 
 
 _RE_IMAGE = re.compile(r'(?P<name>images)\[(?P<width>\d+),\s?(?P<height>\d+)\]')
+_RE_COLORS = re.compile(r'(?P<name>colors)(@(?P<k>\d+))?')
 _RE_PLACES365 = re.compile(r'(?P<name>places365_scenes)\[(?P<model>.*)\]'
                            r'(@(?P<k>\d+))?')
 _RE_COCO = re.compile(r'(?P<name>coco_objects)\[(?P<model>.*)\]'
@@ -312,6 +379,12 @@ class TranslationProcessor:
             width, height = int(d['width']), int(d['height'])
             return ToImageDummyTranslator(width, height)
 
+        color_match = _RE_COLORS.fullmatch(t_spec)
+        if color_match:
+            d = color_match.groupdict()
+            params = {'num_colors': int(d['k'])} if d['k'] else {}
+            return ToColorDistributionTranslator(**params)
+
         places365_match = _RE_PLACES365.fullmatch(t_spec)
         if places365_match:
             d = places365_match.groupdict()
@@ -370,13 +443,13 @@ class TranslationProcessor:
             partial_spark_schema = partial_schema.as_spark_schema()
 
             def translation_iterators(images_batch):
-                for t in translators:
-                    def log_then_call():
-                        self._log_item('Translating to: {}'
-                                       .format(t.output_type_name))
-                        yield from t(images_batch)
+                def log_then_call(t):
+                    self._log_item('Translating to: {}'
+                                   .format(t.output_type_name))
+                    yield from t(images_batch)
 
-                    yield log_then_call()
+                for _t in translators:
+                    yield log_then_call(_t)
 
             def generate_translations(images_batch):
                 stream = zip(*translation_iterators(images_batch))
@@ -423,49 +496,45 @@ class TranslationProcessor:
         return tensor_iter()
 
     def translate(self):
-        try:
-            spark_session = SparkSession.builder \
-                .config('spark.driver.memory',
-                        self.write_cfg.spark_driver_memory) \
-                .config('spark.executor.memory',
-                        self.write_cfg.spark_exec_memory) \
-                .master(self.write_cfg.spark_master) \
-                .getOrCreate()
+        spark_session = SparkSession.builder \
+            .config('spark.driver.memory',
+                    self.write_cfg.spark_driver_memory) \
+            .config('spark.executor.memory',
+                    self.write_cfg.spark_exec_memory) \
+            .master(self.write_cfg.spark_master) \
+            .getOrCreate()
 
-            spark_ctx = spark_session.sparkContext
+        spark_ctx = spark_session.sparkContext
+        spark_ctx.setLogLevel('WARN')
 
+        dfs = [self._translate_to_df(spark_session, spark_ctx,
+                                     t_type, t_list)
+               for t_type, t_list in self.translators_by_type.items()]
 
-            dfs = [self._translate_to_df(spark_session, spark_ctx,
-                                         t_type, t_list)
-                   for t_type, t_list in self.translators_by_type.items()]
+        with materialize_dataset(spark_session, self.output_url, self.schema,
+                                 self.write_cfg.row_group_size_mb):
+            with self._log_task('Joining the dataframes of all '
+                                'translators'):
+                final_df = reduce(partial(DataFrame.join,
+                                          on=self._id_field.name,
+                                          how='inner'), dfs)
+            with self._log_task('Writing to parquet store'):
+                while True:
+                    try:
+                        final_df.write.mode('error')\
+                            .parquet(self.output_url)
+                    except Exception as e:
+                        self._log('Encountered exception: {}'.format(e))
+                        other_url = input('To retry, enter another '
+                                          'output URL and press <Enter>.'
+                                          'To exit, just press <Enter>.')
+                        self.output_url = other_url
+                        if not other_url:
+                            raise e
+                    else:
+                        break
 
-            with materialize_dataset(spark_session, self.output_url, self.schema,
-                                     self.write_cfg.row_group_size_mb):
-                with self._log_task('Joining the dataframes of all '
-                                    'translators'):
-                    final_df = reduce(partial(DataFrame.join,
-                                              on=self._id_field.name,
-                                              how='inner'), dfs)
-                with self._log_task('Writing to parquet store'):
-                    while True:
-                        try:
-                            final_df.write.mode('error')\
-                                .parquet(self.output_url)
-                        except Exception as e:
-                            self._log('Encountered exception: {}'.format(e))
-                            other_url = input('To retry, enter another '
-                                              'output URL and press <Enter>.'
-                                              'To exit, just press <Enter>.')
-                            self.output_url = other_url
-                            if not other_url:
-                                raise e
-                        else:
-                            break
-
-        except KeyboardInterrupt:
-            logging.error('---- ! Stopping due to KeyboardInterrupt ! ----')
-        else:
-            logging.info('----- Finished -----')
+        logging.info('----- Finished -----')
 
 
 if __name__ == '__main__':
