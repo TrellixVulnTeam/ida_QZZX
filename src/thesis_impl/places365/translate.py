c38cf5c31b41c8e35adb20d3176784c19f41b417
Moritz Renftle <moritz.renftle@student.kit.edu>
2020-04-07 21:08:30 +0200

    Start Tensorflow in other processes. Otherwise it is impossible to free GPU memory after running a model.

--- src/thesis_impl/places365/translate.py
+++ src/thesis_impl/places365/translate.py
@@ -2,10 +2,14 @@ import abc
 import argparse
 import csv
 import logging
+import multiprocessing as mp
+import os
+import queue
 import re
 from collections import Counter
-from contextlib import contextmanager
+from contextlib import contextmanager, suppress
 from functools import reduce, partial
+from typing import Optional, Any, Dict, Iterable
 
 import numpy as np
 import torch
@@ -29,7 +33,8 @@ import thesis_impl.places365.config as cfg
 from thesis_impl.util.functools import cached_property
 from thesis_impl.util.webcache import WebCache
 
-import tensorflow as tf
+
+RowDict = Dict[str, Any]
 
 
 class DataGenerator(abc.ABC):
@@ -41,7 +46,6 @@ class DataGenerator(abc.ABC):
         self.output_description = output_description
         self.id_field = UnischemaField('translation_id', np.int64, (),
                                        ScalarCodec(IntegerType()), False)
-
         self._log_nesting = 0
 
     @property
@@ -58,13 +62,6 @@ class DataGenerator(abc.ABC):
         Output schema of this translator as a petastorm `Unischema`.
         """
         return Unischema('TranslatorSchema', [self.id_field] + self.fields)
-
-    @abc.abstractmethod
-    def __call__(self) -> DataFrame:
-        """
-        Generate a dataframe with a schema conforming to `self.schema`.
-        """
-        pass
 
     def _log(self, msg):
         prefix = '--' * self._log_nesting + ' ' if self._log_nesting else ''
@@ -90,50 +87,37 @@ class DataGenerator(abc.ABC):
             self._log_group_end()
 
 
-class BatchedTranslator(DataGenerator, abc.ABC):
-    """
-    Translates batches of input data to other data and collects
-    the results in a dataframe.
-    """
+class DictBasedDataGenerator(DataGenerator):
 
     def __init__(self, spark_session: SparkSession, output_description: str):
         super().__init__(output_description)
         self.spark_session = spark_session
 
     @abc.abstractmethod
-    def batch_iter(self):
-        pass
-
-    @abc.abstractmethod
-    def translate(self, batch):
+    def translate(self) -> Iterable[RowDict]:
+        """
+        Generate a sequence of rows with a schema conforming to `self.schema`.
+        """
         pass
 
     def __call__(self) -> DataFrame:
+        """
+        Generate a dataframe with a schema conforming to `self.schema`.
+        """
         with self._log_task('Translating to: {}'
-                            .format(self.output_description)):
-            dfs = []
-            item_count = 0
+                                    .format(self.output_description)):
+            rows = []
 
-            for batch in self.batch_iter():
-                prev_count = item_count
+            for row_id, row_dict in enumerate(self.translate()):
+                row_dict[self.id_field.name] = row_id
+                rows.append(dict_to_spark_row(self.schema, row_dict))
 
-                rows = []
-                for row_dict in self.translate(batch):
-                    row_dict[self.id_field.name] = item_count
-                    rows.append(dict_to_spark_row(self.schema, row_dict))
-                    item_count += 1
-
-                df = self.spark_session\
-                    .createDataFrame(rows, self.schema.as_spark_schema())
-                dfs.append(df)
-
-                self._log_item('Processed batch of items {}-{}'
-                               .format(item_count, prev_count, item_count - 1))
-
-            return reduce(DataFrame.union, dfs)
+            spark_schema = self.schema.as_spark_schema()
+            df = self.spark_session.createDataFrame(rows, spark_schema)
+            return df
 
 
-class TorchTranslator(BatchedTranslator, abc.ABC):
+class TorchTranslator(DictBasedDataGenerator, abc.ABC):
     """
     Reads batches of image data from a petastorm store
     and converts these images to Torch tensors.
@@ -159,9 +143,14 @@ class TorchTranslator(BatchedTranslator, abc.ABC):
         return unsupervised_loader(self.input_url, self.read_cfg,
                                    self.device)
 
-    def __call__(self):
+    @abc.abstractmethod
+    def translate_batch(self, batch) -> Iterable[RowDict]:
+        pass
+
+    def translate(self):
         with torch.no_grad():
-            return super().__call__()
+            for batch in self.batch_iter():
+                yield from self.translate_batch(batch)
 
 
 class ToImageDummyTranslator(TorchTranslator):
@@ -183,7 +172,7 @@ class ToImageDummyTranslator(TorchTranslator):
     def fields(self) -> [UnischemaField]:
         return [self._field]
 
-    def translate(self, image_tensors):
+    def translate_batch(self, image_tensors):
         for image_tensor in image_tensors.cpu():
             image_arr = np.asarray(to_pil_image(image_tensor, 'RGB')
                                    .resize((self._width, self._height)))
@@ -238,7 +227,7 @@ class ToColorDistributionTranslator(TorchTranslator):
     def fields(self) -> [UnischemaField]:
         return self._fields
 
-    def translate(self, image_tensors):
+    def translate_batch(self, image_tensors):
         for image_tensor in image_tensors.cpu():
             image = to_pil_image(image_tensor, 'RGB')\
                 .resize((self._resize_to, self._resize_to))\
@@ -325,7 +314,7 @@ class ToPlaces365SceneLabelTranslator(TorchModelTranslator):
                                                read_cfg, device, create_model,
                                                top_k=top_k)
 
-    def translate(self, image_tensors):
+    def translate_batch(self, image_tensors):
         probs = self.model(image_tensors)
         _, predicted_labels_batch = torch.topk(probs, self._top_k, -1)
         for predicted_labels in predicted_labels_batch:
@@ -401,7 +390,7 @@ class ToCocoObjectNamesTranslator(TorchModelTranslator):
         return {ToCocoObjectNamesTranslator.object_names()[obj_id]: count
                 for obj_id, count in enumerate(counts_array) if count > 0}
 
-    def translate(self, image_tensors):
+    def translate_batch(self, image_tensors):
         for result in self.model(image_tensors):
             obj_ids, scores = result['labels'], result['scores']
             obj_counts = Counter(obj_id.cpu().item()
@@ -415,7 +404,7 @@ class ToCocoObjectNamesTranslator(TorchModelTranslator):
             yield {self._field.name: counts_arr}
 
 
-class TFTranslator(BatchedTranslator, abc.ABC):
+class TFTranslator(DictBasedDataGenerator, abc.ABC):
     """
     Reads batches of image data from a petastorm store
     and converts these images to Tensorflow tensors.
@@ -435,38 +424,59 @@ class TFTranslator(BatchedTranslator, abc.ABC):
                              shuffle_row_groups=shuffle)
         peta_dataset = make_petastorm_dataset(reader) \
             .batch(self.read_cfg.batch_size)
-        # peta_dataset = self.tf_dist_strategy \  # TODO: not working yet
-        #     .experimental_distribute_dataset(peta_dataset)
 
         for schema_view in peta_dataset:
             yield schema_view.image
 
-    def __call__(self):
-        return super().__call__()
 
-
-class TFModelTranslator(TFTranslator, abc.ABC):
+class TFObjectDetectionProcess(mp.Process):
     """
-    Loads a Tensorflow ML model before consuming data.
-    Useful to load the model first on the GPU before the data.
+    Executes an object detection model in a separate process
+    and delivers the results with a queue.
     """
 
-    def __init__(self, spark_session: SparkSession, input_url: str,
-                 output_description: str, read_cfg: cfg.PetastormReadConfig,
-                 create_model_func):
-        super().__init__(spark_session, input_url, output_description, read_cfg)
+    def __init__(self, model_name: str, cache: WebCache,
+                 in_queue: mp.JoinableQueue, out_queue: mp.Queue,
+                 gpu_id: Optional[int]):
+        super().__init__()
 
-        self._create_model = create_model_func
-        self.model = None
+        self.model_name = model_name
+        self.cache = cache
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+        self.gpu_id = gpu_id
 
-    def __call__(self):
-        # self.tf_dist_strategy = tf.distribute.MirroredStrategy()
-        # with self.tf_dist_strategy.scope():  # TODO: not working yet
-        self.model = self._create_model()  # allocate model before tensors
-        return super().__call__()
+    def run(self):
+        if self.gpu_id:
+            # set GPU id before importing tensorflow
+            os.environ['CUDA_VISIBLE_DEVICES'] = '{}'.format(self.gpu_id)
+
+        # import tensorflow locally in the process
+        import tensorflow as tf
+
+        model_file_name = self.model_name + '.tar.gz'
+        url = ToOIV4ObjectNamesTranslator.MODELS_URL
+
+        self.cache.cache(model_file_name, url, is_archive=True)
+        model_dir = self.cache.get_absolute_path(self.model_name) \
+                    / 'saved_model'
+
+        model = tf.saved_model.load(str(model_dir))
+        model = model.signatures['serving_default']
+
+        while True:
+            in_batch = self.in_queue.get()
+            if in_batch is None:
+                logging.info('Exiting Process for GPU {}'.format(self.gpu_id))
+                self.in_queue.task_done()
+                break
+
+            out_batch = model(in_batch)
+            self.out_queue.put(out_batch)
+            self.in_queue.task_done()
 
 
-class ToOIV4ObjectNameTranslator(TFModelTranslator):
+class ToOIV4ObjectNamesTranslator(TFTranslator):
     """
     Translates each image to a set of detected objects from the OpenImages task.
     The OpenImages task includes 600 objects.
@@ -477,11 +487,21 @@ class ToOIV4ObjectNameTranslator(TFModelTranslator):
     LABELS_URL = 'https://storage.googleapis.com/openimages/2018_04/'
 
     def __init__(self, spark_session: SparkSession, input_url: str,
-                 read_cfg: cfg.PetastormReadConfig, create_model_func,
+                 read_cfg: cfg.PetastormReadConfig, model_name: str,
                  threshold: float, cache: WebCache):
         super().__init__(spark_session, input_url,
-                         'detected objects from the OpenImages set',
-                         read_cfg, create_model_func)
+                         'detected objects from the OpenImages set', read_cfg)
+
+        num_gpus = torch.cuda.device_count()
+        gpus = range(num_gpus) if num_gpus > 0 else (None,)
+
+        self.in_queue = mp.JoinableQueue(maxsize=max(1, num_gpus))
+        self.out_queue = mp.Queue(maxsize=read_cfg.batch_size)
+        self.processes = [TFObjectDetectionProcess(model_name, cache,
+                                                   self.in_queue,
+                                                   self.out_queue,
+                                                   gpu)
+                          for gpu in gpus]
 
         assert 0 <= threshold < 1
         self.threshold = threshold
@@ -497,7 +517,7 @@ class ToOIV4ObjectNameTranslator(TFModelTranslator):
 
     @staticmethod
     def object_names_from_cache(cache: WebCache):
-        url = ToOIV4ObjectNameTranslator.LABELS_URL
+        url = ToOIV4ObjectNamesTranslator.LABELS_URL
 
         with cache.open('class-descriptions-boxable.csv', url)\
                 as object_names_file:
@@ -514,43 +534,56 @@ class ToOIV4ObjectNameTranslator(TFModelTranslator):
                               read_cfg: cfg.PetastormReadConfig,
                               cache: WebCache,
                               threshold: float=.5):
-        def create_model():
-            model_file_name = model_name + '.tar.gz'
-            url = ToOIV4ObjectNameTranslator.MODELS_URL
+        return ToOIV4ObjectNamesTranslator(spark_session, input_url, read_cfg,
+                                           model_name, threshold, cache)
 
-            cache.cache(model_file_name, url, is_archive=True)
-            model_dir = cache.get_absolute_path(model_name) / "saved_model"
+    def batch_iter(self):
+        shuffle = self.read_cfg.shuffle_row_groups
+        reader = make_reader(self.input_url, schema_fields=['image'],
+                             shuffle_row_groups=shuffle)
+        peta_dataset = make_petastorm_dataset(reader) \
+            .batch(self.read_cfg.batch_size)
 
-            model = tf.saved_model.load(str(model_dir))
-            return model.signatures['serving_default']
+        for schema_view in peta_dataset:
+            yield schema_view.image
 
-        return ToOIV4ObjectNameTranslator(spark_session, input_url, read_cfg,
-                                          create_model, threshold, cache)
+    def result_iter(self):
+        for p in self.processes:
+            p.start()
 
-    def translate(self, image_tensors):
-        results = self.model(image_tensors)
+        for batch in self.batch_iter():
+            self.in_queue.put(batch)  # blocks until a slot is free
 
-        obj_ids_batches, scores_batches = results['detection_classes'].numpy(),\
-                                          results['detection_scores'].numpy()
-        for obj_ids, scores in zip(obj_ids_batches, scores_batches):
-            obj_counts = Counter(obj_id for i, obj_id in enumerate(obj_ids)
-                                 if scores[i] > self.threshold)
+            with suppress(queue.Empty):
+                yield self.out_queue.get_nowait()
 
-            obj_ids = range(len(self.object_names()))
-            counts_arr = np.array([obj_counts.get(obj_id, 0)
-                                   for obj_id in obj_ids],
-                                  dtype=np.uint8)
-            yield {self._field.name: counts_arr}
+        for _ in self.processes:
+            self.in_queue.put(None)
+        self.in_queue.join()  # wait until all results are there
 
+        while True:
+            try:
+                yield self.out_queue.get_nowait()
+            except queue.Empty:
+                break
 
-_RE_IMAGE = re.compile(r'(?P<name>images)\[(?P<width>\d+),\s?(?P<height>\d+)\]')
-_RE_COLORS = re.compile(r'(?P<name>colors)(@(?P<k>\d+))?')
-_RE_PLACES365 = re.compile(r'(?P<name>places365_scenes)\[(?P<model>.*)\]'
-                           r'(@(?P<k>\d+))?')
-_RE_COCO = re.compile(r'(?P<name>coco_objects)\[(?P<model>.*)\]'
-                      r'(@(?P<t>0?\.\d+))?')
-_RE_OI = re.compile(r'(?P<name>oi_objects)\[(?P<model>.*)\]'
-                    r'(@(?P<t>0?\.\d+))?')
+        for p in self.processes:
+            p.join()
+
+    def translate(self):
+        for results in self.result_iter():
+            obj_ids_batches = results['detection_classes'].numpy()
+            scores_batches = results['detection_scores'].numpy()
+
+            for obj_ids, scores in zip(obj_ids_batches, scores_batches):
+                obj_counts = Counter(obj_id for i, obj_id in enumerate(obj_ids)
+                                     if scores[i] > self.threshold)
+
+                obj_ids = range(len(self.object_names()))
+                counts_arr = np.array([obj_counts.get(obj_id, 0)
+                                       for obj_id in obj_ids],
+                                      dtype=np.uint8)
+                yield {self._field.name: counts_arr}
 
 
 class JoinDataGenerator(DataGenerator):
@@ -572,6 +605,16 @@ class JoinDataGenerator(DataGenerator):
             dfs = [g() for g in self.generators]
             op = partial(DataFrame.join, on=self.id_field.name, how='inner')
             return reduce(op, dfs)
+
+
+_RE_IMAGE = re.compile(r'(?P<name>images)\[(?P<width>\d+),\s?(?P<height>\d+)\]')
+_RE_COLORS = re.compile(r'(?P<name>colors)(@(?P<k>\d+))?')
+_RE_PLACES365 = re.compile(r'(?P<name>places365_scenes)\[(?P<model>.*)\]'
+                           r'(@(?P<k>\d+))?')
+_RE_COCO = re.compile(r'(?P<name>coco_objects)\[(?P<model>.*)\]'
+                      r'(@(?P<t>0?\.\d+))?')
+_RE_OI = re.compile(r'(?P<name>oi_objects)\[(?P<model>.*)\]'
+                    r'(@(?P<t>0?\.\d+))?')
 
 
 class TranslatorFactory:
@@ -666,7 +709,7 @@ class TranslatorFactory:
             except KeyError:
                 _raise_unknown_model(model_short, d['name'])
             else:
-                return ToOIV4ObjectNameTranslator\
+                return ToOIV4ObjectNamesTranslator\
                     .with_pretrained_model(self.spark_session, model,
                                            self.input_url, self.read_cfg,
                                            self.cache, **params)
