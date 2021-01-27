4e4f9364253a471db1d4d68d984155191090169e
Moritz Renftle <moritz.renftle@kit.edu>
2021-01-27 13:01:50 +0100

    Remove leftovers.

--- src/simadv/oiv4/convert.py
+++ src/simadv/oiv4/convert.py
@@ -1,11 +1,6 @@
-from argparse import ArgumentParser
-
 from simadv.convert import main
 from simadv.oiv4.hub import OpenImagesV4Hub
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Convert a subset of the '
-                                        'OpenImages data to a Parquet'
-                                        'store.')
-    main(parser, OpenImagesV4Hub)
+    main(OpenImagesV4Hub)
--- src/simadv/places365/convert.py
+++ src/simadv/places365/convert.py
@@ -1,11 +1,6 @@
-from argparse import ArgumentParser
-
 from simadv.convert import main
 from simadv.places365.hub import Places365Hub
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Convert a subset of the '
-                                        'Places365 data to a Parquet'
-                                        'store.')
-    main(parser, Places365Hub)
+    main(Places365Hub)
