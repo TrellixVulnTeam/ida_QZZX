ab6378ac2e218eee0da5e1ed1d1a96ad2025916e
Moritz Renftle <moritz.renftle@kit.edu>
2021-01-27 13:09:23 +0100

    Add missing dataclasses.

--- src/simadv/oiv4/convert.py
+++ src/simadv/oiv4/convert.py
@@ -1,6 +1,14 @@
-from simadv.convert import main
+from dataclasses import dataclass, field
+
+from simadv.convert import main, ConvertTask
+from simadv.hub import SupervisedImageDataset
 from simadv.oiv4.hub import OpenImagesV4Hub
 
 
+@dataclass
+class OIV4ConvertTask(ConvertTask):
+    hub: SupervisedImageDataset = field(default=OpenImagesV4Hub, init=False)
+
+
 if __name__ == '__main__':
-    main(OpenImagesV4Hub)
+    main(OIV4ConvertTask)
--- src/simadv/places365/convert.py
+++ src/simadv/places365/convert.py
@@ -1,6 +1,14 @@
-from simadv.convert import main
+from dataclasses import dataclass, field
+
+from simadv.convert import main, ConvertTask
+from simadv.hub import SupervisedImageDataset
 from simadv.places365.hub import Places365Hub
 
 
+@dataclass
+class Places365ConvertTask(ConvertTask):
+    hub: SupervisedImageDataset = field(default=Places365Hub, init=False)
+
+
 if __name__ == '__main__':
-    main(Places365Hub)
+    main(Places365ConvertTask)
