18289af9a1be53afdff8a98b580740ee458b8c05
Moritz Renftle <moritz.renftle@kit.edu>
2021-03-18 15:22:03 +0100

    Add fonts for nicer plotting.

--- src/simexp/plot/accuracy.py
+++ src/simexp/plot/accuracy.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from importlib import resources
+from pathlib import Path
 from typing import List, Optional
 
 import pandas as pd
@@ -6,13 +8,22 @@ from plotnine import *
 
 from simexp.fit import SurrogatesFitter
 
+import matplotlib.font_manager as font_manager
+
+
+font_dirs = list((Path(__file__).parent.parent / 'fonts').iterdir())
+font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
+font_list = font_manager.createFontList(font_files)
+font_manager.fontManager.ttflist.extend(font_list)
+
+
 clear_theme = (theme_bw() +
                theme(panel_border=element_blank(),
                      panel_grid=element_blank(),
                      panel_grid_major=element_blank(),
                      panel_grid_minor=element_blank(),
                      text=element_text(wrap=True,
-                                       family='DejaVu Sans',
+                                       family='Times New Roman',
                                        size=16,
                                        colour='black'),
                      plot_title=element_text(size=20, fontweight='normal'),
