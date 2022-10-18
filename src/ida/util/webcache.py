b04e3ff0d8e66988b9785517d43cf00e4f09e5b7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 12:09:09 +0000

    Adding tarfile member sanitization to extractall()

--- src/ida/util/webcache.py
+++ src/ida/util/webcache.py
@@ -43,7 +43,29 @@ class WebCache:
             self.download(file_name, download_url, download_name)
             if is_archive:
                 with tarfile.open(file_path) as archive_file:
-                    archive_file.extractall(self.cache_dir)
+                    
+                    import os
+                    
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(archive_file, self.cache_dir)
                     file_path.unlink()
                     file_path.touch()  # leave proof that archive was extracted
             return False
